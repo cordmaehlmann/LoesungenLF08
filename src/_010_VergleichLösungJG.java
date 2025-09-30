1bdc467526d85c16765c4e05692da220d190d8c9
Caim89763 <jascha949494@gmx.net>
2025-09-30 09:23:35 +0200

    Meine Lösung

---
+++ src/_010_VergleichLösungJG.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class _010_VergleichLösungJG {
+    public static void main(String[] args) {
+
+        /*
+         * Zwei Zahlen genau vergleichen
+         *
+         * Schreibe ein Programm, das zwei zufällige Zahlen erzeugt.
+         * Dann soll das Programm testen und ausgeben,
+         * welche von den beiden Zahlen größer ist oder ob beide Zahlen gleich groß sind.
+         */
+        Random random = new Random();
+
+
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+        if(a<b){
+            System.out.println(a);
+        }
+        else if (a>b){
+            System.out.println(b);
+        }
+        else if (a==b){
+            System.out.println("Zufälle gibts");
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
+
